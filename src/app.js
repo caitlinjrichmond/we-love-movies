1e947ee53de7d5ec03f7a32250369e453018c2d0
Caitlin Richmond <89586901+caitlinjrichmond@users.noreply.github.com>
2022-01-25 18:01:02 -0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ const moviesRouter = require("./movies/movies.router");
 const reviewsRouter = require("./reviews/reviews.router");
 const theatersRouter = require("./theaters/theaters.router");
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/movies", moviesRouter);
